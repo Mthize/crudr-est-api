0859e0a0898c4755cb44114bb653b21dcd2966c2
Thapelo Mthize <122882874+Mthize@users.noreply.github.com>
2025-05-30 10:12:23 +0200

    creating a postgres database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import epress from "express"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express ();
-const port = process,env.PORT || 3001;
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+// Error Handling middleware
+
+// Server running
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
